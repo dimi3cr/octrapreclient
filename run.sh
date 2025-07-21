b1a0dfe21100391561f7043f46694471e9ef6a53
dimi3cr <dimi3cr7@gmail.com>
2025-07-21 10:02:32 +0000

    push

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 if ! command -v python3 &>/dev/null; then echo "python3 not found"; exit 1; fi
-[ ! -d "venv" ] && python3 -m venv venv
+[ ! -d "venv" ] && python3 -m venv venv9
 source venv/bin/activate
 pip install -r requirements.txt
 python cli.py
